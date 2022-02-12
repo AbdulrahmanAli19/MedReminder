0066e2b4d6c02e368c410370914069580766d6d6
Menna <mennaaaaelsayed@gmail.com>
2022-02-12 23:46:12 +0200

    model of medicien

--- app/src/main/java/com/example/itijavaproject/model/TimesPerDay.java
+++
@@ -1,4 +0,0 @@
-package com.example.itijavaproject.model;
-
-public class TimesPerDay {
-}
--- app/src/main/java/com/example/itijavaproject/model/User.java
+++ app/src/main/java/com/example/itijavaproject/model/User.java
@@ -10,9 +10,65 @@ public class User {
     private String lName;
     private Long birthday;
     private List<User> family;
-    private List<MedicineProgress> medicineProgresses;
     private boolean isMale;
 
+    public User() {
+    }
 
+    public User(int uid, String fName, String lName, Long birthday, List<User> family, boolean isMale) {
+        this.uid = uid;
+        this.fName = fName;
+        this.lName = lName;
+        this.birthday = birthday;
+        this.family = family;
+        this.isMale = isMale;
+    }
 
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public Long getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Long birthday) {
+        this.birthday = birthday;
+    }
+
+    public List<User> getFamily() {
+        return family;
+    }
+
+    public void setFamily(List<User> family) {
+        this.family = family;
+    }
+
+    public boolean isMale() {
+        return isMale;
+    }
+
+    public void setMale(boolean male) {
+        isMale = male;
+    }
 }
