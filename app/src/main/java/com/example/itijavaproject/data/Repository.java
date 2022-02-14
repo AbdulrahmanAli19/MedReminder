e47c4ce6d7d8e1ecb27d08de05d4d828617425dd
abdulrahman.ali19 <abdulrahman.ali19@outlook.com>
2022-02-14 07:58:17 +0200

    adding getInstance() to Repository.java

--- app/src/main/java/com/example/itijavaproject/data/Repository.java
+++ app/src/main/java/com/example/itijavaproject/data/Repository.java
@@ -2,4 +2,14 @@ package com.example.itijavaproject.data;
 
 public class Repository {
 
+    private static Repository repository;
+    private Repository(){}
+
+    public static Repository getInstance(){
+        if (repository == null){
+            repository = new Repository();
+        }
+        return repository;
+    }
+
 }
