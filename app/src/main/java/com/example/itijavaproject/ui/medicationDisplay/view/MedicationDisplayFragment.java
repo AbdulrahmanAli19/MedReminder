3788693367c98082ba73b2dfadb607a3073181ee
radwa <radwamohamed2979@gmail.com>
2022-02-19 23:58:28 +0200

    implementation MedicineDisplayFragment

--- app/src/main/java/com/example/itijavaproject/ui/medicationDisplay/view/MedicationDisplayFragment.java
+++ app/src/main/java/com/example/itijavaproject/ui/medicationDisplay/view/MedicationDisplayFragment.java
@@ -8,38 +8,34 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
 
-import android.text.format.DateFormat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
+import com.example.itijavaproject.R;
 import com.example.itijavaproject.data.db.DatabaseAccess;
 import com.example.itijavaproject.databinding.FragmentMedicationDisplayBinding;
 import com.example.itijavaproject.pojo.model.Medicine;
-import com.example.itijavaproject.ui.medicationDisplay.presenter.MedicineDisplayPresenterInterface;
-import com.example.itijavaproject.ui.medicationsscreen.view.InactiveMedicationAdapter;
-import com.example.itijavaproject.ui.medicationsscreen.view.MedicationsFragment;
-import com.google.type.DateTime;
 
-import java.text.SimpleDateFormat;
+import com.example.itijavaproject.ui.medicationDisplay.presenter.MedicineDisplayPresenterInterface;
+
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.TimeZone;
+
 
 
 public class MedicationDisplayFragment extends Fragment implements MedicineDisplayInterface {
     private static final String TAG = "MedicationDisplayFragme";
     private FragmentMedicationDisplayBinding binding;
     private MedicineDisplayPresenterInterface presenter;
+    NavController navController;
+    NavDirections directions;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,19 +50,33 @@ public class MedicationDisplayFragment extends Fragment implements MedicineDispl
         return binding.getRoot();
     }
 
-    List<Long> listTime = new ArrayList<>();
+
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Medicine medicine= MedicationDisplayFragmentArgs.fromBundle(getArguments()).getObjOfMeds();
-        // binding.drugIcon.setImageResource(Integer.parseInt(medicine.getIconType()));
+        if (medicine.getIconType().equals(getActivity().getApplicationContext().getString(R.string.pill))) {
+            binding.drugIcon.setImageDrawable(getActivity().getApplicationContext().getResources().getDrawable(R.drawable.ic_pill));
+        }
+        else if(medicine.getIconType().equals(getActivity().getApplicationContext().getString(R.string.bottle)))
+        {
+            binding.drugIcon.setImageDrawable(getActivity().getApplicationContext().getResources().getDrawable(R.drawable.ic_bottle_pill));
+        }
+        else if(medicine.getIconType().equals(getActivity().getApplicationContext().getString(R.string.drop)))
+        {
+            binding.drugIcon.setImageDrawable(getActivity().getApplicationContext().getResources().getDrawable(R.drawable.ic_medicine));
+        }
+        else if(medicine.getIconType().equals(getActivity().getApplicationContext().getString(R.string.injection)))
+        {
+            binding.drugIcon.setImageDrawable(getActivity().getApplicationContext().getResources().getDrawable(R.drawable.ic_injection__1_));
+        }
         binding.drugName.setText(medicine.getName());
         binding.noDrugStrength.setText(String.valueOf(medicine.getNoOfStrength()));
         binding.drugStrength.setText(medicine.getStrength());
         binding.txtGetDuration.setText(medicine.getDuration());
-        binding.noPills.setText(String.valueOf(medicine.getNumOfPills()));
+        binding.noPills.append("No.of Pills: "+medicine.getNumOfPills());
         for(int i=0;i<medicine.getTimes().size();i++)
         {
             long millisecondsSinceEpoch = medicine.getTimes().get(i);
@@ -75,19 +85,24 @@ public class MedicationDisplayFragment extends Fragment implements MedicineDispl
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern ( "h:mm a" );
             String output = formatter.format ( zdt );
             binding.txtTimes.append(output+" take 1 pill(s) \n");
-            Log.i(TAG, "onViewCreated: "+output);
-
 
         }
+        DatabaseAccess databaseAccess=DatabaseAccess.getInstance(getActivity().getApplicationContext());
+        binding.deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        databaseAccess.medicineDao().deleteMedicine(medicine);
 
+                    }
+                }).start();
+//                directions=MedicationDisplayFragmentDirections.actionMedicationDisplayFragmentToMedicationsFragment3();
+//                navController.navigate(directions);
 
-//        DatabaseAccess databaseAccess=DatabaseAccess.getInstance(getActivity().getApplicationContext());
-//        binding.deleteBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                //databaseAccess.medicineDao().deleteMedicine();
-//            }
-//        });
+            }
+        });
     }
     @Override
     public void viewMedicine(Medicine medicine) {
